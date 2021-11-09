ec86d18768fd11805e2c2ffa2a70a633514854cd
Artem Tamazov <artem.tamazov@gmail.com>
2021-11-10 00:17:17 +0300

    fix01-issue-internal-4(01) Disable ConvHipImplicitGemmForwardV4R4Xdlops by default for gfx90a/Fp16/Bf16

--- src/solver/conv_hip_implicit_gemm_fwd_v4r4_xdlops.cpp
+++ src/solver/conv_hip_implicit_gemm_fwd_v4r4_xdlops.cpp
@@ -964,8 +964,17 @@ ConvSolution ConvHipImplicitGemmForwardV4R4Xdlops::GetSolution(
 
 bool ConvHipImplicitGemmForwardV4R4Xdlops::IsApplicable(const ConvolutionContext& ctx) const
 {
-    if(miopen::IsDisabled(MIOPEN_DEBUG_CONV_IMPLICIT_GEMM_HIP_FWD_V4R4_XDLOPS{}))
-        return false;
+    const auto device = ctx.GetStream().GetDeviceName();
+    if(device == "gfx90a" && (ctx.IsFp16() || ctx.IsBfp16()))
+    {
+        if(!miopen::IsEnabled(MIOPEN_DEBUG_CONV_IMPLICIT_GEMM_HIP_FWD_V4R4_XDLOPS{}))
+            return false;
+    }
+    else
+    {
+        if(miopen::IsDisabled(MIOPEN_DEBUG_CONV_IMPLICIT_GEMM_HIP_FWD_V4R4_XDLOPS{}))
+            return false;
+    }
 
     if(ctx.skip_solutions_that_take_long_time_to_build_and_have_narrow_coverage)
         return false;
@@ -988,7 +997,7 @@ bool ConvHipImplicitGemmForwardV4R4Xdlops::IsApplicable(const ConvolutionContext
     if(!ctx.Is2d())
         return false;
 
-    if(ctx.GetStream().GetDeviceName() == "gfx90a" && ctx.conv_problem.IsGfx90aFp16altRequired())
+    if(device == "gfx90a" && ctx.conv_problem.IsGfx90aFp16altRequired())
         return false;
 
     if(!IsIndexRangeLargeEnough(ctx))
